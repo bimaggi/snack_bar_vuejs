0cbdbf5eab6972e6f98d0ac1622120f830ed0fa4
bimaggi <bmg.olivier@gmail.com>
2022-01-29 00:05:22 -0300

    feat(Menu):create menu component

---
+++ src/components/SnackBoard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="board">
+    <snack-menu/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import SnackMenu from '@/components/SnackMenu'
+
+export default {
+  name: 'SnackBoard',
+  components: {
+    SnackMenu,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+<style lang="scss">
+.board {
+  margin-top: 0;
+
+}
+
+</style>
---
+++ src/components/SnackMenu.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="menu">
+    <router-link to="/" >Make your request</router-link>
+    <router-link to="/request">Request</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MenuSnack',
+}
+</script>
+
+<style lang="scss">
+  @import '@/styles/variables.scss';
+.menu {
+  padding: 2%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  background-color: $background-body;
+}
+a {
+  font-weight: bold;
+  color: $color1;
+  margin-right: 1%;
+  text-decoration: none;
+  font-size: 1.3rem;
+
+  &.router-link-exact-active {
+    color: $color2;
+  }
+}
+@media screen and (max-width: $mobile){
+  .menu {
+    justify-content:space-around;
+  }
+  a{
+    font-size: 1.1rem;
+  }
+}
+</style>
