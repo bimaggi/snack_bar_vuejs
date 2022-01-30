c4079eb9b78b198af3e50abd6721cb6a4daa65c5
bimaggi <bmg.olivier@gmail.com>
2022-01-29 22:12:22 -0300

    feat(form):create SnackForm component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="footer">
+    <a href="https://www.linkedin.com/in/marcia-b-oliveira/"
+        class="footer__link"
+        target="_blank">
+        <strong>
+          Created and powered by Marcia Bimaggi - 2022
+        </strong>
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+}
+</script>
+
+<style lang="scss">
+ @import '@/styles/variables.scss';
+  .footer {
+    text-align: center;
+    background: $background-body;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .footer__link {
+    letter-spacing: 1px;
+    text-decoration: none;
+    color: $color1;
+  }
+@media screen and (max-width: $mobile){
+  .footer__link{
+    font-size: .7rem;
+  }
+}
+</style>
